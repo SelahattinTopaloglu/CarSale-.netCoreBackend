94f609a4b5bb82584d2173cdf5b7fb3308f6d09d
Selahattin <t.selahattin1998@gmail.com>
2021-05-17 21:53:44 +0300

    key attribute deleted

--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -9,7 +9,6 @@ namespace Entities.Concrete
     public class Customer:IEntity
     {
         public int CustomerId { get; set; }
-        [Key]
         public int UserId { get; set; }
         public string CompanyName { get; set; }
     }
